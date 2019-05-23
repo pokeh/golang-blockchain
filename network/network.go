b035b3b9a8c1a28b9cc34661cb2af7baec1c236f
Chiyo T N <chiyo.nishio@gmail.com>
2019-05-23 13:51:07 -0700

    Add network package

---
+++ network/network.go
@@ -0,0 +1,54 @@
+package network
+
+import "github.com/pokeh/golang-blockchain/blockchain"
+
+const (
+	protocol      = "tcp"
+	version       = 1
+	commandLength = 12
+)
+
+var (
+	nodeAddress     string
+	minerAddress    string
+	KnownNodes      = []string{"localhost:3000"} // this is the central node
+	blocksInTransit = [][]byte{}
+	memoryPool      = make(map[string]blockchain.Transaction)
+)
+
+type Addr struct {
+	AddrList []string
+}
+
+type Block struct {
+	AddrFrom string
+	Block    []byte
+}
+
+type GetBlocks struct {
+	AddrFrom string
+}
+
+type GetData struct {
+	AddrFrom string
+	Type     string
+	ID       []byte
+}
+
+// Inventory
+type Inv struct {
+	AddrFrom string
+	Type     string
+	Items    [][]byte
+}
+
+type Tx struct {
+	AddrFrom    string
+	Transaction []byte
+}
+
+type Version struct {
+	Version    int
+	BestHeight int
+	AddrFrom   string
+}
